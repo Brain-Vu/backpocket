dcbefed3ee4ea7b07c4ea9146b4a01520d0265ba
Brian Vu <brian.vu18@gmail.com>
2025-08-29 14:14:39 -0700

    can create duplicate post its and delete, beginnings of new right click context menu

--- scripts/post-it-logic.js
+++ scripts/post-it-logic.js
@@ -1,14 +1,35 @@
-let postIt = document.getElementById("fred");
-let postItHeader = document.getElementById("beth");
-let moveableArea = document.getElementById("moveable-area");
+let order = 0;
+dragElement("p");
 
-const postItRect = postIt.getBoundingClientRect();
-const postItHeaderRect = postItHeader.getBoundingClientRect();
-const moveableRect = moveableArea.getBoundingClientRect();
+function rightClickElement(event) {
+  event.preventDefault();
 
-dragElement(postIt);
+  let menu = document.getElementById("contextMenu");
 
-function dragElement(element) {
+  document.onclick = hideMenu;
+
+  function hideMenu() {
+    menu.hidden = true;
+  }
+
+  menu.hidden = false;
+  menu.style.left = event.clientX + "px";
+  menu.style.top = event.clientY + "px";
+}
+
+function deleteElement(event) {
+  event.target.parentElement.parentElement.remove();
+}
+
+function dragElement(post_it_id) {
+  const moveableArea = document.getElementById("moveable-area");
+  const moveableRect = moveableArea.getBoundingClientRect();
+
+  let post_it = document.getElementById(post_it_id);
+
+  let postItHeader = post_it.querySelector(".post-it-header");
+  const postItRect = post_it.getBoundingClientRect();
+
   let currX = 0,
     currY = 0,
     offsetX = 0,
@@ -27,8 +48,10 @@ function dragElement(element) {
     currX = e.clientX;
     currY = e.clientY;
 
-    offsetX = e.clientX - parseInt(element.style.left || 0);
-    offsetY = e.clientY - parseInt(element.style.top || 0);
+    offsetX = e.clientX - parseInt(post_it.style.left || 0);
+    offsetY = e.clientY - parseInt(post_it.style.top || 0);
+
+    post_it.style.zIndex = "" + order++;
 
     document.addEventListener("mousemove", mouseDrag);
     document.addEventListener("mouseup", mouseUp);
@@ -37,25 +60,31 @@ function dragElement(element) {
   function mouseDrag(e) {
     e.preventDefault();
 
+    post_it.style.boxShadow = "10px 10px 10px black";
+
     currX = e.clientX;
     currY = e.clientY;
 
     newPosY = currY - offsetY;
     newPosX = currX - offsetX;
 
-    if (newPosY < 0) element.style.top = 0;
+    if (newPosY < 0) post_it.style.top = 0;
     else if (newPosY > maxY - postItHeight)
-      element.style.top = maxY - postItHeight + "px";
-    else element.style.top = newPosY + "px";
+      post_it.style.top = maxY - postItHeight + "px";
+    else post_it.style.top = newPosY + "px";
 
-    if (newPosX < 0) element.style.left = 0;
+    if (newPosX < 0) post_it.style.left = 0;
     else if (newPosX > maxX - postItWidth)
-      element.style.left = maxX - postItWidth + "px";
-    else element.style.left = newPosX + "px";
+      post_it.style.left = maxX - postItWidth + "px";
+    else post_it.style.left = newPosX + "px";
   }
 
   function mouseUp(e) {
+    post_it.style.boxShadow = "2px 2px 5px black";
+
     document.removeEventListener("mousemove", mouseDrag);
     document.removeEventListener("mouseup", mouseUp);
   }
 }
+
+export { dragElement, deleteElement, rightClickElement };
--- scripts/post-it-process.js
+++ scripts/post-it-process.js
@@ -1,19 +1,50 @@
-document
-  .getElementById("allposts")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // stops automatic reloading
+import { dragElement, deleteElement, rightClickElement } from "./post-it-logic.js";
 
-    const formData = new FormData(this);
-    const post1 = formData.get("post1");
-    const post2 = formData.get("post2");
+let num_posts = 0;
+create();
 
-    const posts = [];
-    posts.push(post1, post2);
-    logTs(posts);
-  });
+function create() {
+  let original = document.getElementById("p");
 
-function logTs(posts) {
-  for (const t of posts) {
-    console.log(t);
-  }
+  if (original) {
+    let duplicate = original.cloneNode(true);
+    duplicate.hidden = false;
+    let duplicate_header = duplicate.querySelector(".post-it-header");
+
+    duplicate.id = "p" + num_posts;
+    duplicate_header.id = "ph" + num_posts++;
+
+    duplicate.style.top = "0px";
+    duplicate.style.left = "0px";
+
+    duplicate.querySelector(".post-it-content").value = "";
+    document.getElementById("moveable-area").appendChild(duplicate);
+
+    dragElement(duplicate.id);
+
+    duplicate.addEventListener("contextmenu", rightClickElement)
+    duplicate_header.querySelector(".post-it-exit").addEventListener("click", deleteElement);
+   }
 }
+
+document.getElementById("duplicate-button").addEventListener("click", create);
+
+// document
+//   .getElementById("allposts")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // stops automatic reloading
+
+//     const formData = new FormData(this);
+//     const post1 = formData.get("post1");
+//     const post2 = formData.get("post2");
+
+//     const posts = [];
+//     posts.push(post1, post2);
+//     logTs(posts);
+//   });
+
+// function logTs(posts) {
+//   for (const t of posts) {
+//     console.log(t);
+//   }
+// }
